ef7379c92434fca062947c1dc65b8c878885d807
Maciej Górski <maciek.gorski@gmail.com>
2015-04-29 14:18:24 +0200

    Screen with text and button.

---
+++ app/src/main/groovy/pl/mg6/bob/livecode/MyActivity.java
@@ -0,0 +1,13 @@
+package pl.mg6.bob.livecode;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_activity);
+    }
+}
